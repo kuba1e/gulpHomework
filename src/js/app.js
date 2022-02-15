14ac43e567bb1aecb30e60def1f695b883f817f6
hshse <yakov.petryk@gmail.com>
2022-02-16 00:18:30 +0200

    finished

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var autoplaySlider = $('#autoplay').lightSlider({
+      auto:true,
+      loop:true,
+      autoWidth: true,
+      pause:5000,
+      pauseOnHover: true,
+      
+  });
+});
